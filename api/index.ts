467d8cda22d48b890ca49de5d1d2ce609ca4ac77
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-01-02 03:02:18 +0900

    Implement API

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,48 @@
+import axios from 'axios'
 import express from 'express'
 import pkg from '../package.json'
 
 const app = express()
+
+const surge = 'https://surge.surge.sh'
 
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.send({ name: pkg.name, version: pkg.version })
 })
 
-app.get('/api/token', (req, res) => {
+app.get('/api/token', async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
-  res.send({ message: `Hello` })
+  try {
+    const res2 = await axios.post(`${surge}/token`, {}, {
+      headers: { Authorization: req.headers['authorization'] }
+    })
+    res.send(res2.data)
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const { code, message } = err
+      const { status, statusText } = err.response ?? {}
+      console.error(`${code}: ${status} ${statusText} - ${message}`)
+      res.send({ code, status, statusText, message })
+    }
+  }
 })
 
-app.get('/api/list', (req, res) => {
+app.get('/api/list', async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
-  res.send({ message: `Hello` })
+  try {
+    const res2 = await axios.get(`${surge}/list`, {
+      headers: { Authorization: req.headers['authorization'] }
+    })
+    res.send(res2.data)
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const { code, message } = err
+      const { status, statusText } = err.response ?? {}
+      console.error(`${code}: ${status} ${statusText} - ${message}`)
+      res.send({ code, status, statusText, message })
+    }
+  }
 })
 
 export default app
